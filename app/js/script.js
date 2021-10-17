3c2cde461fc8f9e73db9cbbdb5769d771ea503f6
chazinocencio <89079176+chazinocencio@users.noreply.github.com>
2021-10-17 10:24:12 +0800

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,111 @@
+const heart = document.querySelector(".heart");
+
+heart.addEventListener("click", () =>{
+    heart.classList.toggle("heart-red")
+})
+
+const song1 = document.querySelector(".song1");
+const audio1 = document.querySelector(".audio1");
+
+song1.addEventListener("click", () =>{
+   audio1.classList.toggle("audiosee");
+})
+
+const song2 = document.querySelector(".song2");
+const audio2 = document.querySelector(".audio2");
+
+song2.addEventListener("click", () =>{
+    audio2.classList.toggle("audiosee");
+ })
+
+ 
+const song3 = document.querySelector(".song3");
+const audio3 = document.querySelector(".audio3");
+
+song3.addEventListener("click", () =>{
+    audio3.classList.toggle("audiosee");
+ })
+
+const song4 = document.querySelector(".song4");
+const audio4 = document.querySelector(".audio4");
+
+song4.addEventListener("click", () =>{
+    audio4.classList.toggle("audiosee");
+ })
+
+ const song5 = document.querySelector(".song5");
+const audio5 = document.querySelector(".audio5");
+
+song5.addEventListener("click", () =>{
+    audio5.classList.toggle("audiosee");
+ })
+
+ const song6 = document.querySelector(".song6");
+ const audio6 = document.querySelector(".audio6");
+ 
+ song6.addEventListener("click", () =>{
+     audio6.classList.toggle("audiosee");
+  })
+
+  const song7 = document.querySelector(".song7");
+  const audio7 = document.querySelector(".audio7");
+  
+  song7.addEventListener("click", () =>{
+      audio7.classList.toggle("audiosee");
+   })
+
+   const song8 = document.querySelector(".song8");
+  const audio8 = document.querySelector(".audio8");
+  
+  song8.addEventListener("click", () =>{
+      audio8.classList.toggle("audiosee");
+   })
+
+   const song9 = document.querySelector(".song9");
+  const audio9 = document.querySelector(".audio9");
+  
+  song9.addEventListener("click", () =>{
+      audio9.classList.toggle("audiosee");
+   })
+
+   const song10 = document.querySelector(".song10");
+  const audio10 = document.querySelector(".audio10");
+  
+  song10.addEventListener("click", () =>{
+      audio10.classList.toggle("audiosee");
+   })
+
+   const song11 = document.querySelector(".song11");
+  const audio11 = document.querySelector(".audio11");
+  
+  song11.addEventListener("click", () =>{
+      audio11.classList.toggle("audiosee");
+   })
+
+   const song12 = document.querySelector(".song12");
+   const audio12 = document.querySelector(".audio12");
+   
+   song12.addEventListener("click", () =>{
+       audio12.classList.toggle("audiosee");
+    })
+
+    const song13 = document.querySelector(".song13");
+    const audio13 = document.querySelector(".audio13");
+    
+    song13.addEventListener("click", () =>{
+        audio13.classList.toggle("audiosee");
+     })
+
+     const song14 = document.querySelector(".song14");
+    const audio14 = document.querySelector(".audio14");
+    
+    song14.addEventListener("click", () =>{
+        audio14.classList.toggle("audiosee");
+     })
+
+     const song15 = document.querySelector(".song15");
+     const audio15 = document.querySelector(".audio15");
+     
+     song15.addEventListener("click", () =>{
+         audio15.classList.toggle("audiosee");
+      })
